f4017db0fd7feedef1f364420cd254c2cf06ce2b
Overfitting <75767640+overfitting-ai-community@users.noreply.github.com>
2023-07-12 12:44:27 +0900

    Update app01.py

--- app01.py
+++ app01.py
@@ -8,9 +8,9 @@ st.title("📝DCM comment generator")
 st.text("Situation, Action, Result를 간략하게 입력하면,ChatGPT가 자기평가 comment를 생성합니다.")
 
 with st.form("form"):
-    user_input_situ = st.text_input("Situation")
-    user_input_act = st.text_input("Action")
-    user_input_res = st.text_input("Result")
+    user_input_situ = st.text_input("Situation", "올해 매출 목표는 작년 대비 150%를 달성해야 하는 도전적인 목표였음.")
+    user_input_act = st.text_input("Action", "나는 목표를 달성하기 위해서 세미나에서 신규 고객 발굴하고 기존고객은 지속방문하는 방안을 동원했음.")
+    user_input_res = st.text_input("Result", "올해 목표의 150%를 달성하지 못함. 130% 달성하였음")
     submit = st.form_submit_button("Generate")
 
 if submit and user_input_situ and user_input_act and user_input_res:
